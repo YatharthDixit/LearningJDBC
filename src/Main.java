e338e8cf50bec9b01edffa9143a80b8e76717f44
Yatharth Dixit <yatharthdixit@outlook.com>
2024-09-11 04:55:01 +0530

    added updateName and addStudent functions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws Exception {
         StudentDAO studentDAO = new StudentDAO();
-        Student student = studentDAO.getStudent(4);
+//        studentDAO.addStudent(5, "Kishan");
+        Student student = studentDAO.getStudent(3);
+        System.out.println(student);
+
+        studentDAO.updateName(3, "Sanam");
+         student = studentDAO.getStudent(3);
+        System.out.println(student);
+
+
+
 
     }
 }
@@ -18,11 +27,45 @@ class StudentDAO{
         ResultSet rs = st.executeQuery(query);
         if(rs.next()){
             String name = rs.getString("sname");
-            System.out.println(name);
+//            System.out.println(name);
             s.name = name;
 
         }
         else System.out.println("No student found");
+        st.close();
+        con.close();
         return s;
+
     }
+
+    public boolean addStudent(int roll, String name) throws  Exception{
+
+        Student s = new Student(roll, name);
+        String query = "INSERT INTO students (roll,sname) VALUES (?,?);";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://root@localhost/students","root","root");
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setInt(1, roll);
+        ps.setString(2, name);
+        int res = ps.executeUpdate();
+        ps.close();
+        con.close();
+        return res > 0;
+
+    }
+
+    public boolean updateName(int roll, String name) throws  Exception{
+        String query = "UPDATE Students SET sname = ? WHERE roll = ?";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/students","root","root");
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setInt(2, roll);
+        ps.setString(1, name);
+        int res = ps.executeUpdate();
+        return res>0;
+    }
+
+
+
+
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,21 @@
 public class Student {
      String name;
      int roll;
+
+     Student(){}
+
+     @Override
+     public String toString() {
+          return "Student{" +
+                  "name='" + name + '\'' +
+                  ", roll=" + roll +
+                  '}';
+     }
+
+     Student(int roll, String name){
+          this.roll = roll;
+          this.name = name;
+
+
+     }
 }
